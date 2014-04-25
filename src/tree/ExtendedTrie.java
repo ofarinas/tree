62b3137dcd2e686df60bab4f8630924c07ad090d
osvaldo <osvaldo@Osvaldo-PC>
2014-04-25 12:11:29 +0100

    count cuenta las palabras que hay en el árbol tree

--- src/tree/ExtendedTrie.java
+++ src/tree/ExtendedTrie.java
@@ -9,26 +9,29 @@ package tree;
  * @author ENTRAR
  */
 public class ExtendedTrie extends Trie {
-
+    
     public ExtendedTrie() {
         super(); // Llamada al constructor de la clase Trie
     }
-
+    
+    public int count() {
+        Info aux = root;
+        return countRecursivo(aux);
+    }
+    
     public boolean contain(String cadena) {
         Info temp = root;
         return recursiveContain(temp, cadena);
-
-
     }
-
+    
     private boolean recursiveContain(Info temp, String cadena) {
         if (temp == null) {
             return false;
         }
-        if (temp.c == '*' && cadena.length()==0) {
+        if (temp.c == '*' && cadena.length() == 0) {
             return true;
         }
-        if (cadena.length()==0) {
+        if (cadena.length() == 0) {
             return false;
             
         } else {
@@ -37,6 +40,17 @@ public class ExtendedTrie extends Trie {
             } else {
                 return recursiveContain(temp.alt, cadena);
             }
+        }
+    }
+    
+    private int countRecursivo(Info node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.c == '*') {
+            return countRecursivo(node.alt) + 1;
+        } else {
+            return countRecursivo(node.next) + countRecursivo(node.alt);
         }
     }
 }
